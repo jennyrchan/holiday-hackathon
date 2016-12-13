12b89ff1709826c8b8217053b8eeeb8a5ba2396b
Brandon Culp <bculp513@gmail.com>
2016-12-13 13:58:14 -0500

    customized bones for our app

---
+++ app/main.jsx
@@ -0,0 +1,29 @@
+'use strict'
+import React from 'react'
+import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {render} from 'react-dom'
+import {connect, Provider} from 'react-redux'
+
+// import store from './store'
+import Jokes from './components/Jokes'
+// import Login from './components/Login'
+// import WhoAmI from './components/WhoAmI'
+
+// const ExampleApp = connect(
+//   ({ auth }) => ({ user: auth })
+// ) (
+//   ({ user, children }) =>
+//     <div>
+//       <nav>
+//       </nav> 
+//       {children}
+//     </div>
+// )
+
+render (
+    <Router history={browserHistory}>
+      <Route path="/jokes" component={Jokes}>
+      </Route>
+    </Router>,
+  document.getElementById('main')
+)
---
+++ db/index.js
@@ -0,0 +1,34 @@
+'use strict'
+const debug = require('debug')('sql')
+const chalk = require('chalk')
+const Sequelize = require('sequelize')
+
+
+const url = process.env.DATABASE_URL || 'postgres://localhost:5432/toy_deploy'
+
+console.log(chalk.yellow(`Opening database connection to ${url}`));
+
+// create the database instance
+const db = module.exports = new Sequelize(url, {
+  logging: debug, // export DEBUG=sql in the environment to get SQL queries 
+  native: true,   // lets Sequelize know we can use pg-native for ~30% more speed
+  define: {
+    underscored: true,       // use snake_case rather than camelCase column names
+    freezeTableName: true,   // don't change table names from the one specified
+    timestamps: true,        // automatically include timestamp columns
+  }
+})
+
+// pull in our models
+require('./models')
+
+// sync the db, creating it if necessary
+function sync() {
+  return db.sync({force: true})
+    .then(ok => console.log(`Synced models to db ${url}`))
+    .catch(fail => {
+      console.log(fail)
+    })
+}
+
+db.didSync = sync()
